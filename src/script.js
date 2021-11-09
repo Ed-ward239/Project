9c329dafc01fdda31f74d1c7119e2d0748e476d3
thisisnotdevin <devin1299@outlook.com>
2021-11-09 13:27:17 -0500

    working now

--- src/script.js
+++ src/script.js
@@ -1,23 +1,23 @@
 const sliders = document.querySelectorAll("input[type='range']");
 sliders.forEach(function(slider){
-    slider.addEventListener("input", calculateTip);
+    slider.addEventListener("input",calculateTip);
 });
 //we take it the value shown on the slider and converting it into a input
 
 const billInput = document.getElementById("bill");
-billInput.addEventListener("change", calculateTip);
+billInput.addEventListener("change",calculateTip);
 //registering user input inot our function
 
 function calculateTip(){
     let bill = parseFloat(billInput.value);
     let tipPercent = document.getElementById("tip").value;
-    let TotalPeople = document.getElementById("AmountOfPeople").value;
+    let TotalPeople = document.getElementById("PeopleAmount").value;
     //if the inputs are not conserved as values they will show an error
 
     billInput.value = bill.toFixed(2);
 
-    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2);
-    let total = parseFloat((bill + totalTip)).toFixed(2);
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
 
     let tipPerPerson = (totalTip / TotalPeople).toFixed(2);
     let totalPerPerson = (total / TotalPeople).toFixed(2);
@@ -25,7 +25,7 @@ function calculateTip(){
     document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
     document.getElementById("total-amount").textContent = `\$ ${total}`;
 
-    document.getElementById("tip-percent").textContent = `\$ ${tipPercent}%`;
+    document.getElementById("tip-percent").textContent = `${tipPercent}%`;
     document.getElementById("split-num").textContent = TotalPeople;
 
     document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
